c15e4cc3b37bdfe15b6d5c6cbc45980b46fd90e1
Igor Rodionov <goruha@gmail.com>
2024-11-14 20:22:57 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,19 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "name" {
+  description = "Name of the Global Accelerator."
+  value       = module.global_accelerator.name
+}
+
+output "dns_name" {
+  description = "DNS name of the Global Accelerator."
+  value       = module.global_accelerator.dns_name
+}
+
+output "listener_ids" {
+  description = "Global Accelerator Listener IDs."
+  value       = module.global_accelerator.listener_ids
+}
+
+output "static_ips" {
+  description = "Global Static IPs owned by the Global Accelerator."
+  value       = module.global_accelerator.static_ips
 }
